3ea25d2e02963007b8ccb6f5290ccd1b53976dfd
nalin <devnull@localhost>
2003-06-24 16:38:05 -0500

    - fix path in pam_timestamp_check man page synopsis - make sure the tty is somewhere under /dev when checking it for validity

--- pam_timestamp/pam_timestamp.c
+++ pam_timestamp/pam_timestamp.c
@@ -143,6 +143,11 @@ check_tty(const char *tty)
 	}
 	/* Pull out the meaningful part of the tty's name. */
 	if (strchr(tty, '/') != NULL) {
+		if (strncmp(tty, "/dev/", 5) != 0) {
+			/* Make sure the device node is actually in /dev/,
+			 * noted by Michal Zalewski. */
+			return NULL;
+		}
 		tty = strrchr(tty, '/') + 1;
 	}
 	/* Make sure the tty wasn't actually a directory (no basename). */
